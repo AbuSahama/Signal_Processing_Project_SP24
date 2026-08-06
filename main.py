2ad65ed169b1c699088608941b7811afb56097ff
Abusahama <bsahama7867321@gmail.com>
2026-08-06 15:41:07 +0530

    Complete waveform generation module with six signal types

--- main.py
+++ main.py
@@ -1,43 +1,85 @@
-import numpy as py
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-from signal_generator import generate_sine, generate_square
-
+from signal_generator import (
+    generate_sine,
+    generate_square,
+    generate_triangular,
+    generate_sinc,
+    generate_chirp,
+    generate_cosine
+)
 
 choice = input(
     "Choose a signal:\n"
     "1. Sine\n"
-    "2. Square\n"
+    "2. Cosine\n"
+    "3. Square\n"
+    "4. Triangular\n"
+    "5. Sinc\n"
+    "6. Chirp\n"
     "Enter your choice: "
-)
+).lower()
 
-freq = float(input("Frequency (Hz): "))
+''' Currently, the program directly converts user input using float() and int().
+    If the user enters an invalid value (e.g., "abc" instead of a number),
+    Python raises a ValueError and the application crashes.
+    Before the final testing and submission we should  wrap all user inputs inside try/except
+    blocks (or create reusable helper functions) to catch invalid inputs,
+    display a meaningful error message, and ask the user to enter the value
+    again instead of terminating the program.'''
+
 amp = float(input("Amplitude: "))
 duration = float(input("Duration (s): "))
 sample_rate = int(input("Sampling Rate (Hz): "))
 
-if choice == "1":
-    t, x = generate_sine(freq, amp, duration, sample_rate)
+# Universal Time Vector
+t = np.arange(0, duration, 1 / sample_rate)
 
-elif choice == "2":
+if choice in ["1", "sine"]:
+    freq = float(input("Frequency (Hz): "))
+    phase_ = float(input("Phase Shift (degrees): "))
+    x = generate_sine(t, freq, amp, phase_)
+    title = f"Sine Wave | f={freq} Hz | A={amp} | P={phase_}°"
+
+elif choice in ["2", "cosine"]:
+    freq = float(input("Frequency (Hz): "))
+    phase_ = float(input("Phase Shift (degrees): "))
+    x = generate_cosine(t, freq, amp, phase_)
+    title = f"Cosine Wave | f={freq} Hz | A={amp} | P={phase_}°"
+
+elif choice in ["3", "square"]:
+    freq = float(input("Frequency (Hz): "))
     duty_cycle = float(input("Duty Cycle (%): "))
-    t, x = generate_square(freq, amp, duration, sample_rate, duty_cycle)
+    x = generate_square(t, freq, amp, duty_cycle)
+    title = f"Square Wave | f={freq} Hz | A={amp} | Duty={duty_cycle}%"
+
+elif choice in ["4", "triangular"]:
+    freq = float(input("Frequency (Hz): "))
+    x = generate_triangular(t, freq, amp)
+    title = f"Triangle Wave | f={freq} Hz | A={amp}"
+
+elif choice in ["5", "sinc"]:
+    t = np.arange(-duration/2, duration/2, 1/sample_rate)
+    freq = float(input("Frequency (Hz): "))
+    x = generate_sinc(t, freq, amp)
+    title = f"Sinc Wave | f={freq} Hz | A={amp}"
+
+elif choice in ["6", "chirp"]:
+    freq = float(input("Initial Frequency (Hz): "))
+    f1 = float(input("Final Frequency (Hz): "))
+    t1=duration
+    x = generate_chirp(t, freq, t1, f1, amp)
+    title = (
+    f"Chirp Wave | f₀={freq} Hz | "
+    f"f₁={f1} Hz | T={t1}s | A={amp}"
+    )
+
+
 else:
+    print("Invalid choice!")
     exit()
-
-# titles = {
-#     "1": "Sine Wave",
-#     "2": "Square Wave"
-# }
-
-titles = {
-    "1": f"Sine Wave | f={freq} Hz | A={amp}",
-    "2": f"Square Wave | f={freq} Hz | A={amp} | Duty={duty_cycle}%"
-}
-
-
-
 
 
 # for testing 
@@ -58,8 +100,7 @@ titles = {
     
 
 plt.plot(t,x)
-#plt.plot("Sine Wave")
-plt.title(titles[choice])
+plt.title(title)
 
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
--- signal_generator.py
+++ signal_generator.py
@@ -3,14 +3,37 @@ from scipy import signal
 import matplotlib.pyplot as plt
 
 # Function to generate Sine signal 
-def generate_sine(freq, amp, duration, sample_rate):
-    t=np.arange(0, duration, 1/sample_rate)
-    x=amp*np.sin(2*np.pi*freq*t)
-    return t, x
+# def generate_sine(freq, amp, duration, sample_rate):
+#     t=np.arange(0, duration, 1/sample_rate)
+#     x=amp*np.sin(2*np.pi*freq*t)
+#     return t, x
+def generate_sine(t, freq, amp, phase_=0):
+    phase_ = np.deg2rad(phase_)
+    x = amp * np.sin(2 * np.pi * freq * t + phase_)
+    return x
+
+def generate_cosine(t, freq, amp, phase_=0):
+    phase_ = np.deg2rad(phase_)
+    x = amp * np.cos(2 * np.pi * freq * t + phase_)
+    return x
+
 
 # Function to generate Square signal 
-def generate_square(freq, amp, duration, sample_rate, duty_cycle):
-    t=np.arange(0, duration, 1/sample_rate)
+def generate_square(t, freq, amp, duty_cycle):
     x=amp*signal.square(2*np.pi*freq*t, duty=duty_cycle/100)
-    return t, x
+    return x
+
+def generate_triangular(t, freq, amp):
+    x=amp*signal.sawtooth(2*np.pi*freq*t, width=0.5)
+    return x
+
+def generate_sinc(t, freq, amp):
+    x=amp*np.sinc(t * freq)
+    return x
+
+def generate_chirp(t, freq, t1, f1, amp):
+    x=amp*signal.chirp(t, freq, t1, f1, method='linear')
+    return x
+
+    
 
